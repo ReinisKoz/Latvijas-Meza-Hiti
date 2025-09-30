2e289814315557af20973619ed0f04db3fcbb1b4
22DP2EStep <131154520+22DP2EStep@users.noreply.github.com>
2025-09-30 07:25:16 +0000

    e

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
-        @vite(['resources/js/app.js'])
-        <script src="{{ asset('resources/js/scripts.js') }}"></script>
-    </head>
-    <body>
-        <div id="app">
-            <example-component></example-component>
-        </div>
-    </body>
+<head>
+    <title>Laravel Vite</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div id="app">
+        <example-component></example-component>
+    </div>
+</body>
 </html>
